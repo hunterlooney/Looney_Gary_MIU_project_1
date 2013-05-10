8b4c387181b704f4129be4190911f92cffd96922
Gary W. H. Looney <hunterlooney@fullsail.edu>
2013-05-09 21:57:58 -0400

    Looks good

---
+++ index.js
@@ -0,0 +1,24 @@
+//Gary W. Hunter Looney
+//Project 1
+//Add Character Java
+//Term 1305
+
+
+var goTo = document.createElement('a');
+goTo.href = "additem.html";
+
+var genderButton = document.getElementById('genderPic');
+genderButton.addEventListener("click", goTo);
+
+var raceButton = document.getElementById('racePic');
+raceButton.addEventListener("click", goTo);
+
+var heightButton = document.getElementById('heightPic');
+heightButton.addEventListener("click", goTo);
+
+var birthdateButton = document.getElementById('birthdatePic');
+birthdateButton.addEventListener("click", goTo);
+
+var glassButton = document.getElementById('glassPic');
+glassButton.addEventListener("click", goTo);
+
